cc1ebb78361a881cfbb9618d2e5190e81ed90d99
PORTATIL\Usuario <ismaeltorresmede@gmail.com>
2023-09-26 14:01:48 +0200

    Codigo sencillo

--- Proyecto java/src/App.java
+++ Proyecto java/src/App.java
@@ -1,5 +1,9 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        System.out.println("Buenas tardes");
+        for(int i = 0; i <= 12; i++)
+        {
+            System.out.print("12 * "+ i + " = " + 12 * i + "\n");
+        }
     }
 }
